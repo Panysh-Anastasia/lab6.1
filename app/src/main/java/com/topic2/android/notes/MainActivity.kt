b415298db1ac99f8f850272ab394362068b3d71d
Anastasia <3778242nastya@gmail.com>
2024-03-28 20:19:28 +0300

    created screen notes and realizd potoka date

--- app/src/main/java/com/topic2/android/notes/MainActivity.kt
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -4,17 +4,10 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.runtime.rememberCoroutineScope
-import com.topic2.android.notes.routing.Screen
 import com.topic2.android.notes.theme.NotesTheme
-import com.topic2.android.notes.ui.components.AppDrawer
-import com.topic2.android.notes.ui.components.Note
+import com.topic2.android.notes.ui.components.screens.NotesScreen
 import com.topic2.android.notes.viewmodel.MainViewModel
 import com.topic2.android.notes.viewmodel.MainViewModelFactory
-import kotlinx.coroutines.launch
 
 /**
  * Main activity приложения.
@@ -33,27 +26,10 @@ class MainActivity : AppCompatActivity() {
 
     setContent {
       NotesTheme {
-        val coroutineScope = rememberCoroutineScope()
-        val scaffoldState: ScaffoldState = rememberScaffoldState()
-        Scaffold(
-          scaffoldState = scaffoldState,
-          drawerContent = {
-            AppDrawer(
-              currentScreen = Screen.Notes,
-              closeDrawerAction = {
-                coroutineScope.launch {
-                  scaffoldState.drawerState.close()
-                }
-              }
-            )
-          },
-          content = {
-            Note()
-          }
-        )
+        NotesScreen(viewModel = viewModel)
+
+
       }
-
-
     }
   }
 }
